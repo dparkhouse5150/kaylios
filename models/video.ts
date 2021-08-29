e06ead17b17ae80e9e3aefe4cbf361ab6a48cf7a
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-08-29 10:10:25 -0500

    added the published variable to determine if the video has been published

--- models/video.ts
+++ models/video.ts
@@ -3,7 +3,7 @@ import {User} from "./user";
 export interface Video {
     id?: string;
     title?: string;
-    user: User;
     describe?: string;
     uploadedDate?: Date;
+    published: boolean
 }
