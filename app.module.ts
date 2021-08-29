53717b805409953c4621def91da4f50a1089d88c
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-08-29 12:36:38 -0500

    added the FormsModule

--- app.module.ts
+++ app.module.ts
@@ -7,18 +7,22 @@ import {VideoComponent} from './video/video.component';
 import {AngularFireModule} from "@angular/fire/compat";
 import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
 import {environment} from '../environments/environment';
+import {VideoDetailsComponent} from './video-details/video-details.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
     declarations: [
         AppComponent,
-        VideoComponent
+        VideoComponent,
+        VideoDetailsComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         AngularFireModule,
         AngularFirestoreModule,
-        AngularFireModule.initializeApp(environment.firebaseConfig)
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
