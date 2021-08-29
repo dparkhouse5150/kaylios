6ab20c0b5db485a66ac0d16efa0109696dbfcc2e
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-08-29 17:01:25 -0500

    VideoComponent, VideoComponent and the AddVideo routes

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,11 +1,21 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {VideoComponent} from './video/video.component';
+import {VideoDetailsComponent} from "./video-details/video-details.component";
+import {AddVideoComponent} from './add-video/add-video.component';
 
 const routes: Routes = [
     {
-        path: '',
+        path: '/video',
         component: VideoComponent
+    },
+    {
+        path: '/video-detail/:id',
+        component: VideoDetailsComponent
+    },
+    {
+        path: '/video/add',
+        component: AddVideoComponent
     }
 ];
 
