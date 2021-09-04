4b5adf7f439494300b2370c0198d1888ded90a97
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-09-04 13:24:22 -0500

    added a couple of routes

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,15 +1,27 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {VideoComponent} from './video/video.component';
-import {VideoDetailsComponent} from "./video-details/video-details.component";
-import {AddVideoComponent} from './add-video/add-video.component';
-import {VideoPlaylistComponent} from './video-playlist/video-playlist.component';
+import {VideoDetailsComponent} from "./video/video-details/video-details.component";
+import {AddVideoComponent} from './video/add-video/add-video.component';
+import {VideoPlaylistComponent} from './video/video-playlist/video-playlist.component';
 
 const routes: Routes = [
 
     {
         path: '',
         component: VideoPlaylistComponent
+    },
+    {
+        path: '/video',
+        component: VideoComponent
+    },
+    {
+        path: '/video/details/:id',
+        component: VideoDetailsComponent
+    },
+    {
+        path: '/video/add',
+        component: AddVideoComponent
     }
 ];
 
