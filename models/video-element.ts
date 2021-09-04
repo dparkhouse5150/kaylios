3ed28b6d329abe10eeb606e23b176dd9028ad56f
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-09-04 13:22:53 -0500

    basic interface for the video element component

---
+++ models/video-element.ts
@@ -0,0 +1,9 @@
+export interface VideoElement {
+    /* returns an id of the element */
+    id: string
+
+    /* returns the node name. only available on native HTML tags */
+    readonly nodeName?: string
+
+
+}
