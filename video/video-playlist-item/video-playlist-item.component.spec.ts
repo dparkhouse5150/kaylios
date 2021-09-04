5f14a1f455018ad7baf53516cd05cd168c97e4e0
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-09-04 12:58:51 -0500

    unit testing for the component

---
+++ video/video-playlist-item/video-playlist-item.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VideoPlaylistItemComponent } from './video-playlist-item.component';
+
+describe('VideoPlaylistItemComponent', () => {
+  let component: VideoPlaylistItemComponent;
+  let fixture: ComponentFixture<VideoPlaylistItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VideoPlaylistItemComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VideoPlaylistItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
