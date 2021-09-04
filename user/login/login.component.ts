b48a1747cce20d38dc5e7cbec08cf672edac490e
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-09-04 17:09:45 -0500

    added the logut method

--- user/login/login.component.ts
+++ user/login/login.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { SocialAuthService, GoogleLoginProvider, SocialUser } from 'angularx-social-login';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    loginForm: FormGroup
+    socialUser: SocialUser
+    isLoggedIn: boolean
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private FormBuilder: FormBuilder,
+        private socialAuthService: SocialAuthService,
+    ) { }
 
+    ngOnInit(): void {
+        this.loginForm = this.FormBuilder.group({
+            email: ['', Validators.required],
+            password: ['', Validators.required],
+        })
+
+        this.socialAuthService.authState.subscribe((user) => {
+            this.socialUser = user
+            this.isLoggedIn = (user !== null)
+
+            console.log(this.socialUser)
+        })
+    }
+
+    loginWithGoogle = ():void => {
+        this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)
+    }
+
+    logout = ():void => {
+        this.socialAuthService.signOut()
+    }
 }
