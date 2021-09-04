0fcd5cf4af8b0ac21f6e2be6d53436fda19675d3
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-09-04 13:00:52 -0500

    logic for the component

---
+++ video/video-playlist/video-playlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-video-playlist',
+  templateUrl: './video-playlist.component.html',
+  styleUrls: ['./video-playlist.component.scss']
+})
+export class VideoPlaylistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
