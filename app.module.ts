376a30521e8c8d26488b072933b1a76ca0505d9f
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-09-04 13:10:07 -0500

    refactored a few componetns

--- app.module.ts
+++ app.module.ts
@@ -7,14 +7,25 @@ import {VideoComponent} from './video/video.component';
 import {AngularFireModule} from "@angular/fire/compat";
 import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
 import {environment} from '../environments/environment';
-import {VideoDetailsComponent} from './video-details/video-details.component';
+import {VideoDetailsComponent} from './video/video-details/video-details.component';
 import {FormsModule} from "@angular/forms";
-import {VideoListComponent} from './video-list/video-list.component';
-import {AddVideoComponent} from './add-video/add-video.component';
-import {VideoPlaylistComponent} from './video-playlist/video-playlist.component';
-import {VideoPlaylistItemComponent} from './video-playlist-item/video-playlist-item.component';
+import {VideoListComponent} from './video/video-list/video-list.component';
+import {AddVideoComponent} from './video/add-video/add-video.component';
+import {VideoPlaylistComponent} from './video/video-playlist/video-playlist.component';
+import {VideoPlaylistItemComponent} from './video/video-playlist-item/video-playlist-item.component';
+
+import {ReactiveFormsModule} from '@angular/forms';
+
+import {SocialLoginModule, SocialAuthServiceConfig, GoogleLoginProvider} from 'angularx-social-login';
 
 import {VgCoreModule} from '@videogular/ngx-videogular/core';
+import {VgControlsModule} from "@videogular/ngx-videogular/controls";
+import {VgOverlayPlayModule} from "@videogular/ngx-videogular/overlay-play";
+import {VgBufferingModule} from "@videogular/ngx-videogular/buffering";
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+// import { MediaDirective } from './directives/media.directive'
+
 
 @NgModule({
     declarations: [
@@ -24,7 +35,11 @@ import {VgCoreModule} from '@videogular/ngx-videogular/core';
         VideoListComponent,
         AddVideoComponent,
         VideoPlaylistComponent,
-        VideoPlaylistItemComponent
+        VideoPlaylistItemComponent,
+        LoginComponent,
+        RegisterComponent,
+        // MediaDirective
+
     ],
     imports: [
         BrowserModule,
@@ -33,9 +48,23 @@ import {VgCoreModule} from '@videogular/ngx-videogular/core';
         AngularFirestoreModule,
         AngularFireModule.initializeApp(environment.firebaseConfig),
         FormsModule,
-        VgCoreModule
+        ReactiveFormsModule,
+        VgCoreModule,
+        SocialLoginModule,
+        VgControlsModule,
+        VgOverlayPlayModule,
+        VgBufferingModule
     ],
-    providers: [],
+    providers: [{
+        provide: 'SocialAuthServiceConfig',
+        useValue: {
+            autoLogin: false,
+            providers: [{
+                id: GoogleLoginProvider.PROVIDER_ID,
+                provider: new GoogleLoginProvider('your-client-id-here')
+            }] 
+        } as SocialAuthServiceConfig,
+    }],
     bootstrap: [AppComponent]
 })
 export class AppModule {
