7f1cc206380b39669335cdca9109ca86d3486b8e
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-09-05 08:05:11 -0500

    added the loginWithGoogle method

--- user/login/login.component.ts
+++ user/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user';
 import { SocialAuthService, GoogleLoginProvider, SocialUser } from 'angularx-social-login';
-
+import {titleLengthChecker} from '../../validation"'
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
@@ -10,26 +10,20 @@ import { SocialAuthService, GoogleLoginProvider, SocialUser } from 'angularx-soc
 })
 export class LoginComponent implements OnInit {
 
-    loginForm: FormGroup
+    // loginForm;
     socialUser: SocialUser
     isLoggedIn: boolean
 
     constructor(
-        private FormBuilder: FormBuilder,
+        private loginForm: FormGroup,
         private socialAuthService: SocialAuthService,
-    ) { }
+    ) { 
+        
+    }
 
     ngOnInit(): void {
-        this.loginForm = this.FormBuilder.group({
-            email: ['', Validators.required],
-            password: ['', Validators.required],
-        })
+        this.loginForm = new FormGroup({
 
-        this.socialAuthService.authState.subscribe((user) => {
-            this.socialUser = user
-            this.isLoggedIn = (user !== null)
-
-            console.log(this.socialUser)
         })
     }
 
@@ -38,6 +32,6 @@ export class LoginComponent implements OnInit {
     }
 
     logout = ():void => {
-        this.socialAuthService.signOut()
+        this.socialAuthService.signOut
     }
 }
